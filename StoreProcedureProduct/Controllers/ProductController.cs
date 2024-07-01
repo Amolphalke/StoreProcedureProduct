815e901c4cfccb14457d9470f71b248c7798748b
Amol Phalke <amolp.anvitechsys@gmail.com>
2024-07-01 12:59:41 +0530

    Add project files.

---
+++ StoreProcedureProduct/Controllers/ProductController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoreProcedureProduct.Data;
+using StoreProcedureProduct.Models;
+
+namespace StoreProcedureProduct.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly Product_Repository _repository;
+        public ProductController(Product_Repository repository)
+        {
+            _repository = repository;
+        }
+        // GET: ProductController
+        public async Task <IActionResult> Index()
+        {
+            List<Product> products = await _repository.GetProductsAsync();
+            return View(products);
+        }
+
+        // GET: ProductController/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await _repository.GetProductByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _repository.AddProductAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: ProductController/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await _repository.GetProductByIdAsync(id);
+            if (product == null) return NotFound();
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _repository.UpdateProductAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: ProductController/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _repository.GetProductByIdAsync(id);
+            if (product == null) return NotFound();
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _repository.DeleteProductAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ StoreProcedureProduct/Data/Product_Repository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using StoreProcedureProduct.Models;
+
+namespace StoreProcedureProduct.Data
+{
+    public class Product_Repository
+    {
+        private readonly ApplicationDbContext _context;
+        public Product_Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Product>> GetProductsAsync()
+        {
+            return await _context.Products.FromSqlRaw("EXEC spGetProducts").ToListAsync();
+        }
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            var parameter = new SqlParameter("@ProductId", id);
+            var products = await _context.Products.FromSqlRaw("EXEC spGetProductById @ProductId", parameter).ToListAsync();
+            return products.FirstOrDefault();
+        }
+        public async Task AddProductAsync(Product product)
+        {
+            var parameters = new[]
+            {
+                new SqlParameter("@Name", product.Name),
+                new SqlParameter("@Price", product.Price)
+            };
+            await _context.Database.ExecuteSqlRawAsync("EXEC spAddProduct @Name, @Price", parameters);
+        }
+        public async Task UpdateProductAsync(Product product)
+        {
+            var parameters = new[]
+            {
+                new SqlParameter("@ProductId", product.ProductId),
+                new SqlParameter("@Name", product.Name),
+                new SqlParameter("@Price", product.Price)
+            };
+            await _context.Database.ExecuteSqlRawAsync("EXEC spUpdateProduct @ProductId, @Name, @Price", parameters);
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            var parameter = new SqlParameter("@ProductId", id);
+            await _context.Database.ExecuteSqlRawAsync("EXEC spDeleteProduct @ProductId", parameter);
+        }
+    }
+}
